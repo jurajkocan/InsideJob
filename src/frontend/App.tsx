9a9743c4aab6f52880583be0cc250b7eff2ad233
jurajkocan <jurajkocan@windowslive.com>
2020-05-14 13:33:35 +0200

    intl provider for translations

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -4,9 +4,21 @@ import { store } from "./redux/Store";
 import { RouterApp } from "./Router";
 import { cssRaw } from "typestyle";
 import { cssForIe } from "src/style/common";
+import { IntlProvider } from "react-intl";
+import translation_en_US from "src/translations/en-US.json";
+import translation_wookie from "src/translations/wookie.json";
+
 cssRaw(cssForIe);
+
+const usersLocale = "en-US";
+const translations = {
+  "en-US": translation_en_US,
+  wookie: translation_wookie,
+};
 export default (
-  <Provider store={store}>
-    <RouterApp />
-  </Provider>
+  <IntlProvider locale={usersLocale} messages={translations[usersLocale]}>
+    <Provider store={store}>
+      <RouterApp />
+    </Provider>
+  </IntlProvider>
 );
